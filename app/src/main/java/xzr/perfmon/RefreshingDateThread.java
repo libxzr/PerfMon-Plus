d7a9a16bd4027fcee87ae97215f9931c686d8281
LibXZR <xzr467706992@163.com>
2020-01-29 23:11:33 +0800

    java: RefreshingDateThread: Don't refresh data if user disabled it
    * Save power, lock issue.

--- app/src/main/java/xzr/perfmon/RefreshingDateThread.java
+++ app/src/main/java/xzr/perfmon/RefreshingDateThread.java
@@ -27,24 +27,34 @@ public class RefreshingDateThread extends Thread {
         while (!FloatingWindow.do_exit){
                 for (int i=0;i<cpunum;i++){
                     cpuonline[i]=JniTools.getcpuonlinestatus(i);
-                    if(cpuonline[i]==1)
+                    if(cpuonline[i]==1&&FloatingWindow.show_cpufreq_now)
                     cpufreq[i]=JniTools.getcpufreq(i);
                 }
-                adrenofreq=JniTools.getadrenofreq();
-                adrenoload=JniTools.getadrenoload();
-                mincpubw=JniTools.getmincpubw();
-                cpubw=JniTools.getcpubw();
-                m4m=JniTools.getm4m();
-                maxtemp=JniTools.getmaxtemp();
-                memusage=JniTools.getmemusage();
-                current=JniTools.getcurrent();
-                gpubw=JniTools.getgpubw();
-                llcbw=JniTools.getllcbw();
+                if(FloatingWindow.show_gpufreq_now)
+                    adrenofreq=JniTools.getadrenofreq();
+                if(FloatingWindow.show_gpuload_now)
+                    adrenoload=JniTools.getadrenoload();
+                if(FloatingWindow.show_mincpubw_now)
+                    mincpubw=JniTools.getmincpubw();
+                if(FloatingWindow.show_cpubw_now)
+                    cpubw=JniTools.getcpubw();
+                if(FloatingWindow.show_m4m_now)
+                    m4m=JniTools.getm4m();
+                if(FloatingWindow.show_thermal_now)
+                    maxtemp=JniTools.getmaxtemp();
+                if(FloatingWindow.show_mem_now)
+                    memusage=JniTools.getmemusage();
+                if(FloatingWindow.show_current_now)
+                    current=JniTools.getcurrent();
+                if(FloatingWindow.show_gpubw_now)
+                    gpubw=JniTools.getgpubw();
+                if(FloatingWindow.show_llcbw_now)
+                    llcbw=JniTools.getllcbw();
                 if(reverse_current_now)
                     current=-current;
                 FloatingWindow.ui_refresher.sendEmptyMessage(0);
                 for (int i=0;i<cpunum;i++){
-                    if(cpuonline[i]==1) {
+                    if(cpuonline[i]==1&&FloatingWindow.show_cpuload_now) {
                         final int ii=i;
                         new Thread() {
                             public void run() {
